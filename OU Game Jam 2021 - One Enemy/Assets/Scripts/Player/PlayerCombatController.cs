b72666a34b61601aaea29479f259913cac2dba59
Kenny Mead <kennethmead784@gmail.com>
2021-05-01 13:08:16 -0400

    Adjusted player prefab

--- OU Game Jam 2021 - One Enemy/Assets/Scripts/Player/PlayerCombatController.cs
+++ OU Game Jam 2021 - One Enemy/Assets/Scripts/Player/PlayerCombatController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerCombatController : MonoBehaviour
 {
     [SerializeField]
     private int health = 10;
     [SerializeField]
-    private GameObject weapon = null;
+    private GameObject RightArm = null;
     private float attackDelay = 0.5f;
+
+    Vector2 aim;
+
+    [SerializeField] Transform Gun;
+
+    public void LookListener(CallbackContext context)
+    {
+        aim = context.ReadValue<Vector2>();
+        
+    }
 
     public IEnumerator Attack()
     {
